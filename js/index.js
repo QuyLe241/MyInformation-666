245056749c63208f7fc56430a866bd875a6f4615
QUY666666 <lenhatquy2401@gmail.com>
2024-11-07 16:11:24 +0700

    update 7/11 4H

--- js/index.js
+++ js/index.js
@@ -28,25 +28,49 @@
 //   list.innerHTML += items; // Nhân đôi danh sách để tạo vòng lặp
 // });
 
-document.addEventListener("DOMContentLoaded", () => {
-  const list = document.querySelector(".list");
-  const items = document.querySelectorAll(".item");
-  const itemWidth = items[0].offsetWidth + 20; // Tính chiều rộng của mỗi item (bao gồm margin)
+//    test2
 
-  let position = 0; // Vị trí ban đầu của cuộn
+// document.addEventListener("DOMContentLoaded", () => {
+//   const list = document.querySelector(".list");
+//   const items = document.querySelectorAll(".item");
+//   const itemWidth = items[0].offsetWidth + 20; // Tính chiều rộng của mỗi item (bao gồm margin)
 
-  function startSlider() {
-    position -= 2; // Điều chỉnh tốc độ bằng cách thay đổi giá trị này
+//   let position = 0; // Vị trí ban đầu của cuộn
 
-    // Kiểm tra xem item đầu tiên đã đi hết khỏi khung nhìn chưa
-    if (Math.abs(position) >= itemWidth) {
-      position = 0; // Reset lại vị trí
-      list.appendChild(list.firstElementChild); // Di chuyển item đầu tiên xuống cuối
-    }
+//   function startSlider() {
+//     position -= 2; // Điều chỉnh tốc độ bằng cách thay đổi giá trị này
 
-    list.style.transform = `translateX(${position}px)`;
-    requestAnimationFrame(startSlider); // Lặp lại hiệu ứng cuộn
+//     // Kiểm tra xem item đầu tiên đã đi hết khỏi khung nhìn chưa
+//     if (Math.abs(position) >= itemWidth) {
+//       position = 0; // Reset lại vị trí
+//       list.appendChild(list.firstElementChild); // Di chuyển item đầu tiên xuống cuối
+//     }
+
+//     list.style.transform = `translateX(${position}px)`;
+//     requestAnimationFrame(startSlider); // Lặp lại hiệu ứng cuộn
+//   }
+
+//   startSlider();
+// });
+
+// //    back to top
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 300 ||
+    document.documentElement.scrollTop > 300
+  ) {
+    document.getElementById("backToTop").style.display = "block";
+  } else {
+    document.getElementById("backToTop").style.display = "none";
   }
+}
 
-  startSlider();
-});
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
