8bcfb028f7331acb3617f674ab25b097e7f0e81f
QUY666666 <lenhatquy2401@gmail.com>
2025-02-22 06:28:12 +0700

    update animatione dots 22/2

--- js/index.js
+++ js/index.js
@@ -1,58 +1,3 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   const list = document.querySelector(".list");
-//   const items = document.querySelectorAll(".item");
-//   const itemWidth = items[0].offsetWidth + 20; // Tính chiều rộng của mỗi item (bao gồm margin)
-
-//   let position = 0; // Vị trí ban đầu của cuộn
-
-//   function startSlider() {
-//     position -= 2; // Điều chỉnh tốc độ bằng cách thay đổi giá trị này
-
-//     // Khi vị trí đạt đến hết một item, đưa nó trở lại cuối danh sách
-//     if (Math.abs(position) >= itemWidth) {
-//       position = 0;
-//       list.appendChild(list.firstElementChild); // Di chuyển item đầu tiên xuống cuối
-//     }
-
-//     list.style.transform = `translateX(${position}px)`;
-//     requestAnimationFrame(startSlider); // Lặp lại hiệu ứng cuộn
-//   }
-
-//   startSlider();
-// });
-
-//      test
-// document.addEventListener("DOMContentLoaded", () => {
-//   const list = document.querySelector(".list");
-//   const items = list.innerHTML; // Lấy nội dung HTML hiện tại của danh sách
-//   list.innerHTML += items; // Nhân đôi danh sách để tạo vòng lặp
-// });
-
-//    test2
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   const list = document.querySelector(".list");
-//   const items = document.querySelectorAll(".item");
-//   const itemWidth = items[0].offsetWidth + 20; // Tính chiều rộng của mỗi item (bao gồm margin)
-
-//   let position = 0; // Vị trí ban đầu của cuộn
-
-//   function startSlider() {
-//     position -= 2; // Điều chỉnh tốc độ bằng cách thay đổi giá trị này
-
-//     // Kiểm tra xem item đầu tiên đã đi hết khỏi khung nhìn chưa
-//     if (Math.abs(position) >= itemWidth) {
-//       position = 0; // Reset lại vị trí
-//       list.appendChild(list.firstElementChild); // Di chuyển item đầu tiên xuống cuối
-//     }
-
-//     list.style.transform = `translateX(${position}px)`;
-//     requestAnimationFrame(startSlider); // Lặp lại hiệu ứng cuộn
-//   }
-
-//   startSlider();
-// });
-
 // //    back to top
 window.onscroll = function () {
   scrollFunction();
@@ -77,7 +22,7 @@ function scrollToTop() {
 // const textsAbout = ["Lê Nhất Quý", "A Web Developer"];
 
 const textElement = document.getElementById("text");
-const textsAbout = ["Lê Nhất Quý", "a Web Developer"];
+const textsAbout = ["LE NHAT QUY", "a Web Developer"];
 let currentIndex = 0;
 let charIndex = 0;
 let isDeleting = false;
